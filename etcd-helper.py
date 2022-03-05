60d7b6e5edb5fd1ab1b76547a079180d09b74f25
YuShin <spencer810704@gmail.com>
2022-03-05 20:02:36 +0800

    Update etcd-helper.py

--- etcd-helper.py
+++ etcd-helper.py
@@ -8,16 +8,17 @@ class EtcdTools:
     def __init__(self, hostname, port, username, password) -> None:
         self.etcd = etcd3.client(host=hostname, port=port, user=username, password=password)
 
-    def add_host_dns_record(self, env: str, wl_code: str, hostname: str, ip_address: str):
+
+    def add_host_dns_record(self, env: str, wl_code: str, host: str, ip_address: str):
 
         dns_record_ttl = 60
 
         # 因prod環境與其他環境命名方式不是統一 , 由傳入的env參數進行判斷
         if env == "prod":
-            etcd_key = f"/coredns/nexiosoft/{wl_code}/{hostname}"
+            etcd_key = f"/coredns/nexiosoft/{wl_code}/{host}"
 
         elif env == "uat" or env == "stg" or env == "sit":
-            etcd_key = f"/coredns/nexiosoft/{env}/{wl_code}/{hostname}"
+            etcd_key = f"/coredns/nexiosoft/{env}/{wl_code}/{host}"
 
         # 序列化
         etcd_value = json.dumps({"host": f"{ip_address}", "ttl": dns_record_ttl})
@@ -29,16 +30,15 @@ class EtcdTools:
         print("寫入etcd")
         self.etcd.put(key=etcd_key, value=etcd_value)
         
-    def delete_host_dns_record(self, env: str, wl_code: str, hostname: str):
+    def delete_host_dns_record(self, env: str, wl_code: str, host: str):
         
-        hostname = hostname.split(".")[0]
 
         # 因prod環境與其他環境命名方式不是統一 , 由傳入的env參數進行判斷
         if env == "prod":
-            etcd_key = f"/coredns/nexiosoft/{wl_code}/{hostname}"
+            etcd_key = f"/coredns/nexiosoft/{wl_code}/{host}"
 
         elif env == "uat" or env == "stg" or env == "sit":
-            etcd_key = f"/coredns/nexiosoft/{env}/{wl_code}/{hostname}"
+            etcd_key = f"/coredns/nexiosoft/{env}/{wl_code}/{host}"
 
         # 寫入etcd
         print("刪除etcd")
@@ -48,18 +48,17 @@ class EtcdTools:
         
 
 if __name__ == "__main__":
-    hostname = sys.argv[1]
-    port = sys.argv[2]
-    username = sys.argv[3]
-    password = sys.argv[4]
+    etcd_hostname = sys.argv[1]
+    etcd_port = sys.argv[2]
+    etcd_username = sys.argv[3]
+    etcd_password = sys.argv[4]
     
     env = sys.argv[5].lower()
     wl_code = sys.argv[6].lower()    
     host = sys.argv[7].lower()
     ip_address = sys.argv[8].lower()
     
-    etcd_tool = EtcdTools(hostname=hostname, port=port, username=username, password=password)
+    etcd_tool = EtcdTools(hostname=etcd_hostname, port=etcd_port, username=etcd_username, password=etcd_password)
 
     # 增加解析
-    etcd_tool.add_host_dns_record(env=env, wl_code=wl_code, hostname=host, ip_address=ip_address)
-
+    etcd_tool.add_host_dns_record(env=env, wl_code=wl_code, host=host, ip_address=ip_address)
